8fe45e65d670e4d09865e23aa1b38687a5f965ed
Elena Grebenchuk <elena_grebenchuk@mai.ru>
2022-12-09 17:11:20 +0300

    solved the tasks

---
+++ conditionals/index.js
@@ -0,0 +1,59 @@
+// 1. Создать программу в которой сравнивается возраст
+// двух человек (Федота и Митрофана).
+// Если возраст Федота больше чем возраст Митрофана,
+// то должно выводится сообщение, в котором указана
+// разница в возрасте: «Федот старше Митрофана на 3 ».
+// Если возраст Митрофана больше, то выводится такое
+// сообщение: «Митрофан старше Федота на 5».
+// Если возраст обоих человек один и тот же, то
+// выводится такое сообщение: «Федот и Митрофан
+// одного возраста».
+
+// let firstAge = prompt('enter the age of Mitrofan');
+// let secondAge = prompt('enter the age of Fedot');
+
+// if (firstAge === null || secondAge === null) {
+//     alert('you canselled');
+// } else {
+//     firstAge = Number(firstAge);
+//     secondAge = Number(secondAge);
+// }
+//  if (isNaN(firstAge) || isNaN(secondAge)) {
+//     alert('enter a numbers');
+//  }else if(firstAge > secondAge) {
+//     alert('Mitrofan is 5 years older than Fedot');
+//  }else if(firstAge < secondAge) {
+//     alert('Fedot is 3 years older than Mitrofan');
+//  }else {
+//     alert('Fedot and Mitrofan are the same age');
+//  }
+
+
+//  2. Пользователь вводит ТРИ числа
+// вывести в консоль наибольшее из них
+
+// let numberOne = prompt('enter number one');
+// let numberTwo = prompt('enter number two');
+// let numberThree = prompt('enter number three')
+
+// if (numberOne === null || numberTwo === null || numberThree === null) {
+//     alert('you canselled');
+// } else {
+//     numberOne = Number(numberOne);
+//     numberTwo = Number(numberTwo);
+//     numberThree = Number(numberThree);
+// }
+//  if (isNaN(numberOne) || isNaN(numberTwo) || isNaN(numberThree)) {
+//     console.log('enter a numbers');
+//  }else if(numberOne > numberTwo && numberOne > numberThree) {
+//     console.log(numberOne);
+
+//  }else if(numberTwo > numberOne && numberTwo > numberThree) {
+//     console.log(numberTwo);
+
+// }else if(numberThree > numberOne && numberThree > numberTwo) {
+//     console.log(numberThree);
+// }else {
+    // console.log('numbers are equal');
+// }
+
