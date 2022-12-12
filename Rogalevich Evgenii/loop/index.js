500f5cd7d0c10bd2fa2297e5a5848909962f8498
Evgeniy Rogalevich <zhenyok.r@tut.by>
2022-12-12 22:39:06 +0300

    loop, three task

--- Rogalevich Evgenii/loop/index.js
+++ Rogalevich Evgenii/loop/index.js
@@ -1,25 +1,25 @@
 // 1. Написать программу, которая вычисляет сумму чисел от 1 до n. Значение n вводится с клавиатуры.
 // Проверить число n на корректность (чтобы это было число, чтобы оно не было меньше 1, чтобы это было не null).
 
-let n = prompt("Введите число n");
+// let userNumber = prompt("Введите число n");
 
-if (n === null) {
-  alert("Вы вышли из программы");
-} else if (n == 0) {
-  alert("Число n должно быть больше 0");
-} else if (!isNaN(n)) {
-  n = Number(n);
-  console.log(`OK LET WORK! User number is ${n}`);
-} else {
-  alert("Введите число");
-}
-let step = 0;
-let result = 0;
-while (step < n) {
-  step++;
-  result = result + step;
-}
-alert(`Сумма чисел от 1 до ${n} : ${result}`);
+// if (userNumber === null) {
+//   alert("Вы вышли из программы");
+// } else if (userNumber == 0) {
+//   alert("Число n должно быть больше 0");
+// } else if (!isNaN(userNumber)) {
+//   userNumber = Number(userNumber);
+//   console.log(`OK LET WORK! User number is ${userNumber}`);
+// } else {
+//   alert("Введите число");
+// }
+// let step = 0;
+// let result = 0;
+// while (step < userNumber) {
+//   step++;
+//   result = result + step;
+// }
+// alert(`Сумма чисел от 1 до ${userNumber} : ${result}`);
 
 // 2. Написать программу, которая определяет количество
 // положительных, отрицательных чисел и нулей. В
@@ -31,12 +31,48 @@ alert(`Сумма чисел от 1 до ${n} : ${result}`);
 // Отрицательных: 5 шт.
 // Нулей: 3 шт.»
 
+// const userNumbers = prompt("Какое количество чисел Вы собираетесь определить?");
+// let userNumber = [];
+// for (i = 0; i < userNumbers; i++) {
+//   userNumber[i] = Number(prompt(`Введите ${i + 1} число`));
+// }
+// let positiveNumber = 0;
+// let negativeNumber = 0;
+// let nullNumber = 0;
+
+// for (i = 0; i < userNumber.length; i++) {
+//   if (userNumber[i] > 0) {
+//     positiveNumber++;
+//   } else if (userNumber[i] < 0) {
+//     negativeNumber++;
+//   } else {
+//     nullNumber++;
+//   }
+// }
+// alert(`Положительных: ${positiveNumber} шт.
+// Отрицательных: ${negativeNumber} шт.
+// Нулей: ${nullNumber} шт.`);
+
 // 3. Написать программу, которая выводит на экран
 // двузначные числа, которые делятся на 4, но не
 // делятся на 6.
+
+// let userResult = [];
+// let i = 0;
+// for (n = 10; n <= 99; n++) {
+//   if (n % 4 === 0 && n % 6 !== 0) {
+//     i++;
+//     userResult[i] = n;
+//   }
+// }
+// alert(userResult);
 
 // 4. Написать программу, которая выводит среднее
 // арифметическое n чисел введенных пользователем.
 // Ввод чисел прекращается
 // тогда, когда нажали «отмена», после чего выводится
 // среднее арифметическое.
+
+let someNumber = [];
+do {}
+console.log(someNumber);
