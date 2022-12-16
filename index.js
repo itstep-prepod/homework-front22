3bb0e498ae0deb6c2aa5ad495bf510d80ed7b701
ArhiSV <sergis13@mail.ru>
2022-12-16 16:14:35 +0300

    add hw files

--- index.js
+++ index.js
@@ -9,6 +9,35 @@
 // выводится такое сообщение: «Федот и Митрофан
 // одного возраста».
 
-let
+let ageMitrofan = prompt('Enter age Mitrofan');
+let ageFedot = prompt('Enter age Fedot');
+
+ageMitrofan =Number(ageMitrofan);
+ageFedot = Number(ageFedot);
+
+if(ageFedot > ageMitrofan){
+   console.log('Федот старше Митрофана на 3')
+} else if (ageFedot < ageMitrofan){
+   console.log('Митрофан старше Федота на 5')
+}else {
+   console.log('Федот и Митрофан одного возраста')
+}
+
 // 2. Пользователь вводит ТРИ числа
 // вывести в консоль наибольшее из них
+
+// let aNumber = prompt('enter 1st number')
+// let bNumber = prompt('enter 2nd number')
+// let cNumber = prompt('enter 3rd number')
+
+// aNumber = Number(aNumber)
+// bNumber = Number(bNumber)
+// cNumber = Number(cNumber)
+
+// if (aNumber > bNumber){
+//     console.log(aNumber);
+// }else if(bNumber > cNumber ){
+//    console.log(bNumber);
+// } else {
+//     console.log(cNumber);
+//  } 
